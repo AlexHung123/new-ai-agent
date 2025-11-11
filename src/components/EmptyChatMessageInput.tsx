be9d724b25c3686ee2d73a9b6e4b9a28620919f1
alexhong <yifenghung123@gmail.com>
2025-11-11 18:08:51 +0800

    Feat: Add All Agents Page

--- src/components/EmptyChatMessageInput.tsx
+++ src/components/EmptyChatMessageInput.tsx
@@ -64,8 +64,8 @@ const EmptyChatMessageInput = () => {
           className="px-2 bg-transparent placeholder:text-[15px] placeholder:text-black/50 dark:placeholder:text-white/50 text-sm text-black dark:text-white resize-none focus:outline-none w-full max-h-24 lg:max-h-36 xl:max-h-48"
           placeholder="Ask anything..."
         />
-        <div className="flex flex-row items-center justify-between mt-4">
-          <Optimization />
+        <div className="flex flex-row items-center justify-end mt-4">
+          {/* <Optimization /> */}
           <div className="flex flex-row items-center space-x-2">
             <div className="flex flex-row items-center space-x-1">
               <ModelSelector />
